637e3e955b54e371229ee53aa16948ac951465cf
Vadzim <40643436+Vadimvr@users.noreply.github.com>
2022-12-03 18:00:48 +0300

    Added Database and Entity Framework

---
+++ db/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using models;
+
+namespace db
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        { }
+
+        public DbSet<Record> Records { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Record>().HasIndex(u => u.Id).IsUnique();
+        }
+    }
+}
